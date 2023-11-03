ae8edad9480573b1aca8508251f9feb95d02805a
Yash-271120 <yashpatil.yp2711@gmail.com>
2023-11-03 08:15:08 +0530

    [ADD]: movie->hall grpc communication

--- backend/services/hall/controller/hall.py
+++ backend/services/hall/controller/hall.py
@@ -1,6 +1,7 @@
 import time
 import random
 from proto import hallService_pb2, hallService_pb2_grpc, movieService_pb2
+import grpc
 from db.model import hall
 from db import db
 
@@ -18,19 +19,37 @@ class HallService(hallService_pb2_grpc.HallServiceServicer):
 
     def GetHall(self, request, context):
         id = request.id
-        fake_movie = movieService_pb2.Movie(id=1, title="fake_movie")
-        fake_capacity = 100
-        fake_available = random.randint(0, 100)
-        hall = hallService_pb2.Hall(id=id, movie=fake_movie, capacity=fake_capacity, available=fake_available)
-        return hallService_pb2.GetHallResponse(hall=hall)
+        db_session = db.get_session()
+        hallObj = hall.Hall().find_by_id(db_session, id)
+        if(hallObj == None):
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("hall not found")
+            return hallService_pb2.GetHallResponse()
+        res_movie = movieService_pb2.Movie(id=hallObj.movie_id, title=hallObj.movie_name)
+        res_capacity = hallObj.capacity
+        res_available = hallObj.available
+        hall_pb = hallService_pb2.Hall(id=id, movie=res_movie, capacity=res_capacity, available=res_available)
+        return hallService_pb2.GetHallResponse(hall=hall_pb)
 
     def UpdateHallAvailable(self, request, context):
         id = request.id
-        available = request.available
-        fake_movie = movieService_pb2.Movie(id=1, title="fake_movie")
-        fake_capacity = 100
-        hall = hallService_pb2.Hall(id=id, movie=fake_movie, capacity=fake_capacity, available=available)
-        return hallService_pb2.UpdateHallAvailableResponse(hall=hall)
+        db_session = db.get_session()
+        hallObj = hall.Hall().find_by_id(db_session, id)
+        if(hallObj == None):
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("hall not found")
+            return hallService_pb2.GetHallResponse()
+        res_available = request.available
+        if(hallObj.available-res_available < 0):
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("available seats not enough")
+            return hallService_pb2.UpdateHallAvailableResponse()
+        hallObj.available = hallObj.available - res_available
+        hallObj.update(session=db_session)
+        res_movie = movieService_pb2.Movie(id=hallObj.movie_id, title=hallObj.movie_name)
+        res_capacity = hallObj.capacity
+        hall_pb = hallService_pb2.Hall(id=id, movie=res_movie, capacity=res_capacity, available=res_available)
+        return hallService_pb2.UpdateHallAvailableResponse(hall=hall_pb)
 
 def getHallService():
     return HallService()
