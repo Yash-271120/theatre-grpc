cb69b3f9f36aa1fd05ff609bed5ffb7e876f6a35
Yash-271120 <yashpatil.yp2711@gmail.com>
2023-11-03 08:10:02 +0530

    [ADD]: added timestamps

--- backend/services/hall/db/model/hall.py
+++ backend/services/hall/db/model/hall.py
@@ -2,10 +2,11 @@ from sqlalchemy.orm import relationships
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
-from sqlalchemy import String,Integer
+from sqlalchemy import String,Integer,select, DateTime
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from datetime import datetime
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -17,6 +18,8 @@ class Hall(Base):
     capacity = mapped_column('capacity', Integer,nullable=False)
     available = mapped_column('available', Integer,nullable=False)
     movie_name = mapped_column('movie_name', String,nullable=False)
+    created_at = mapped_column('created_at', DateTime,nullable=False,default=datetime.now())
+    updated_at = mapped_column('updated_at', DateTime,nullable=False,default=datetime.now(),onupdate=datetime.now())
 
     def __repr__(self) -> str:
         return f"Hall(id={self.id},movie_id={self.movie_id},capacity={self.capacity},available={self.available},movie_name={self.movie_name})"
@@ -34,3 +37,7 @@ class Hall(Base):
         session.delete(self)
         session.commit()
         return self
+    def find_by_id(self,session,id):
+        stmt = select(Hall).where(Hall.id==id)
+        hall = session.scalar(stmt)
+        return hall
